590700937b2937048ba95996d385d96adfc2d40e
HarshRaghav17 <harsh@nxt.pe>
2024-02-14 23:02:08 +0530

    first commit-for deploy

---
+++ script.js
@@ -0,0 +1,10 @@
+// let dayNight=document.querySelector(".dayNight");
+// let banner= document.querySelector(".banner");
+
+// dayNight.addEventListener("click",()=>{
+//     banner.classList.toggle("night");
+// })
+document.getElementById('contact-button').addEventListener('click', function() {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+  });
+  
